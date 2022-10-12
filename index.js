aa7ae34f302c77ba3dcfab798887287a1acd4762
Johnny <johnnyturco@me.com>
2022-10-12 09:42:07 -0500

    commit tree 466caddd328858bc5ef4ef957e4044d7182247eb

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Code your solutions in this file
+const messages = [];
+
+function writeCards(names, event) {
+  for (let i = 0; i < names.length; i++) {
+    messages.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`)
+    debugger;
+  }
+  return messages;
+}
+
+
+function countDown(positiveInteger) {
+  let i = positiveInteger;
+  while (i >= 0) {
+    console.log(i);
+    i--;
+  }
+}
